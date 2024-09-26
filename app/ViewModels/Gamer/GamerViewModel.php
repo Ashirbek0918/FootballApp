cf741c1c2674a641269bd653025323ba3fcf2b6b
Ashirbek0918 <ashirbekrajapboyev@gmail.com>
2024-09-26 18:44:17 +0500

    added

--- app/ViewModels/Gamer/GamerViewModel.php
+++ app/ViewModels/Gamer/GamerViewModel.php
@@ -22,9 +22,6 @@ class GamerViewModel extends BaseViewModel
     protected function populate():void
     {
         $this->created_at = Carbon::parse($this->created_at)->format('d-m-Y');
-        if(count($this->files) >0 ){
-
-            $this->file = $this->files[0];
-        }
+        $this->file = !empty($this->files) ? $this->files[0] : null;
     }
 }
