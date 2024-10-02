b87881cdd7afdc0df973c448fa03f9aa8632506a
Ashirbek0918 <ashirbekrajapboyev@gmail.com>
2024-10-02 15:17:22 +0500

    added

--- config/cors.php
+++ config/cors.php
@@ -16,19 +16,12 @@ return [
     */
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    'allowed_methods' => ['http://localhost:3000'],
-
-    'allowed_origins' => ['*'],
-
+    'allowed_methods' => ['*'],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://127.0.0.1:8000')],
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
